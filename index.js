c77a033df357e6f5aa031b31742acc68f16c5c4c
Emiliano Acerbi <eacerbip@gmail.com>
2022-05-16 10:23:51 -0300

    progress

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// const { application } = require('express')
 const express = require('express')
 const app = express()
 
@@ -22,6 +23,24 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
